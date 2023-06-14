d2618b06a775c145c0b8cb7487db3929b8fa68c5
Suleman Soren <chintutudu2@gmail.com>
2023-06-15 00:40:40 +0530

    Project setup for lint and project structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'import-glob',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        cwd: 'babelrc',
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@app': './src/App',
+          '@components': './src/Components',
+          '@screens': './src/Screens',
+        },
+      },
+    ],
+  ],
 };
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module '*.json';
+
+declare module '@app/*';
+declare module '@components/*';
+declare module '@screens/*';
